00656b32b22bc64cff79005ceac12a92cb723ef0
ck-saurabh <saurabh@thechefkart.com>
2021-10-02 19:14:35 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var fs = require("fs");
-// var pdf = require("html-pdf");
+var html_to_pdf = require("html-pdf-node");
 var html = fs.readFileSync("./index.html", "utf8");
-// var options = { format: "A4" };
 
-// pdf.create(html, options).toFile("./invoice.pdf", function (err, res) {
-//   if (err) return console.log(err);
-//   console.log(res);
-// });
-const { jsPDF } = require("jspdf");
+let options = { format: "A4", path: "invoice.pdf" };
 
-// Default export is a4 paper, portrait, using millimeters for units
-const doc = new jsPDF();
-
-doc.html(html, { filename: "a4.pdf", autoPaging: true });
+let file = { content: html };
+html_to_pdf.generatePdf(file, options).then((pdfBuffer) => {
+  console.log("PDF Buffer:-", pdfBuffer);
+});
